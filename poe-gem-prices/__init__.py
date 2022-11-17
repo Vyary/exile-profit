65fa9c74e1f52ab47098c2acda2886605c6924bc
Vyary <vyaryw@gmail.com>
2022-11-17 04:10:14 +0200

    setup.py and __init__.py added

---
+++ poe-gem-prices/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+
+# Version of poe-gem-prices
+__version__ = '1.0.0'
---
+++ setyp.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='poe-gem-prices',
+    version='1.0.0',
+    description='This is a python script for scraping prices for gems in path of exile',
+    author='Velizar Arnaudov',
+    author_email='vyaryw@gmail.com',
+    url='https://github.com/Vyary/poe-gem-prices',
+    install_requires=['requests', 'pandas', 'tqdm'],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'poe-gem-prices = poe-gem-prices.poe-gem-prices:main',
+        ],
+    },
+)
