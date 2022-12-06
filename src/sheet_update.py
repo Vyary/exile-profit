71a436f5c4a46dca2dc5176b1f2773315b5c2599
Vyary <vyaryw@gmail.com>
2022-12-06 05:02:05 +0200

    Updated gems.csv

--- src/sheet_update.py
+++ src/sheet_update.py
@@ -2,7 +2,10 @@ import gspread
 import pandas as pd
 
 
-def iter_pd(df):
+def iter_pd(df: pd.DataFrame):
+    """
+    Yields the values of a Pandas DataFrame, handling missing values.
+    """
     for val in df.columns:
         yield val
     for row in df.to_numpy():
@@ -13,23 +16,44 @@ def iter_pd(df):
                 yield val
 
 
-def pandas_to_sheets(pandas_df, sheet, clear=True):
-    # Updates all values in a workbook to match a pandas dataframe
+def pandas_to_sheets(
+    pandas_df: pd.DataFrame, sheet: gspread.Worksheet, clear: bool = True
+) -> None:
+    """
+    Updates all values in a worksheet to match a Pandas DataFrame.
+    """
+    # Clear the worksheet if specified
     if clear:
         sheet.clear()
-    (row, col) = pandas_df.shape
-    cells = sheet.range(
-        "A1:{}".format(gspread.utils.rowcol_to_a1(row + 1, col))  # type:ignore
-    )
+
+    # Get the shape of the DataFrame
+    (row_count, col_count) = pandas_df.shape
+
+    # Calculate the end cell of the worksheet range
+    end_cell = gspread.utils.rowcol_to_a1(row_count + 1, col_count)
+
+    # Get the cells in the worksheet range
+    cells = sheet.range("A1:{}".format(end_cell))
+
+    # Update the cell values with the values from the DataFrame
     for cell, val in zip(cells, iter_pd(pandas_df)):
         cell.value = val
+
+    # Update the worksheet with the new cell values
     sheet.update_cells(cells)
 
 
 def main():
-    gc = gspread.service_account("output/service_account.json")  # type:ignore
-    workbook = gc.open("Poe gem prices")
+    # Get a Google Sheets client using a service account
+    sheets_client = gspread.service_account("output/service_account.json")
+
+    # Open the workbook
+    workbook = sheets_client.open("Poe gem prices")
+
+    # Read the CSV file into a Pandas DataFrame
     df = pd.read_csv("output/gems.csv")
+
+    # Update the worksheet with the values from the DataFrame
     pandas_to_sheets(df, workbook.worksheet("Poe gem prices"))
 
 
